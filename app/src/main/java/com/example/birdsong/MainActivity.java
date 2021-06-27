619ca8a18473db80610a00bf9f725f7ac5f50c69
Chris <chris00dardar@gmail.com>
2021-06-26 22:01:52 -0500

    Initial commit: splash screen with fade in

---
+++ app/src/main/java/com/example/birdsong/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.birdsong;
+
+import android.os.Bundle;
+
+import com.example.birdsong.ui.main.SectionsPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+}
---
+++ app/src/main/java/com/example/birdsong/ui/main/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.birdsong.ui.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.birdsong.MainActivity;
+import com.example.birdsong.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        ImageView logo = (ImageView)findViewById(R.id.logo);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.animation);
+        logo.startAnimation(fadeIn);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        },2000);
+
+    }
+}
